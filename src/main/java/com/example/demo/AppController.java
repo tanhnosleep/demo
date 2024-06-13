4792a3aaf1bd31d578bcfd57ed44e97e1980643f
tanhnosleep <toilatanh7@gmail.com>
2024-06-13 15:54:37 +0700

    Xử lý nhập sai phần price

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Controller
 public class AppController {
@@ -32,8 +37,13 @@ public class AppController {
 	}
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String saveProduct(@ModelAttribute("product") Product product) {
+	public String saveProduct(@Validated @ModelAttribute("product") Product product, BindingResult result,RedirectAttributes ra){
+		if (result.hasErrors() || product.getPrice() <= 0.0) {
+			ra.addFlashAttribute("message", "Giá tiền không hợp lệ! ");
+            return "redirect:/";
+        }
 		service.save(product);
+		ra.addFlashAttribute("message", "The product has been saved successfully");
 		return "redirect:/";
 	}
 	
@@ -46,8 +56,9 @@ public class AppController {
 	}
 	
 	@RequestMapping("/delete/{id}")
-	public String deleteProduct(@PathVariable(name="id") int id) {
+	public String deleteProduct(@PathVariable(name="id") int id, RedirectAttributes ra) {
 		service.delete(id);
+		ra.addFlashAttribute("message", "The product has been deleted successfully");
 		return "redirect:/";
 	}
 }
