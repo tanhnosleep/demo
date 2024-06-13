360f6cdedc2d4a1360e8a7db9242043c101e5777
tanhnosleep <toilatanh7@gmail.com>
2024-06-13 13:11:24 +0700

    CRUD product with Spring boot, mySQL and thymeleaf

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String brand;
+	private float price;
+	
+	
+	public Product() {
+		super();
+	}
+	
+	
+	public Product(int id, String name, String brand, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+	}
+
+
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
---
+++ src/main/java/com/example/demo/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll(){
+		return repo.findAll();
+	}
+	
+	public void save(Product product) {
+		repo.save(product);
+	}
+	
+	public Product get(int id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(int id) {
+		repo.deleteById(id);
+	}
+
+}
